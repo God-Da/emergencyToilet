6a330a202bef8726739b0c67b8ae162ac3c54430
God-Da <150757592+God-Da@users.noreply.github.com>
2025-10-02 01:12:25 +0900

    first setting

---
+++ backend/src/main/java/com/day/emergency/toilet/EmergencyToiletApplication.java
@@ -0,0 +1,13 @@
+package com.day.emergency.toilet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyToiletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyToiletApplication.class, args);
+	}
+
+}
---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>테스트 중</p>
+    </>
+  );
+}
+
+export default App;
