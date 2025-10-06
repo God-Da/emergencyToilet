6d2b9f1d9337339f9eb68bb6c7895507a7049776
God-Da <150757592+God-Da@users.noreply.github.com>
2025-10-06 16:35:03 +0900

    front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// 테스트할 컴포넌트
+import Header from "./components/header";
 
-  return (
-    <>
-      <p>테스트 중</p>
-    </>
-  );
+function App() {
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
